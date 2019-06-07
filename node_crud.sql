7286f0027a8adac126d70ea7854b5b65de014826
Sumon sarker <csesumonpro@gmail.com>
2019-06-08 05:53:12 +0600

    sql and readme update

---
+++ node_crud.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jun 07, 2019 at 11:41 PM
+-- Server version: 5.7.24
+-- PHP Version: 7.2.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `node_crud`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `phone`) VALUES
+(1, 'sumon', 'sumon@gmail.com', '0188234234'),
+(4, 'lastone update', 'lastone@gmail.com', '0161160643'),
+(5, 'Apple Iphone', 'adminadmin@gmail.com', '0129416544');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ readme.md
@@ -0,0 +1,21 @@
+<p align="center"><img src="https://user-images.githubusercontent.com/29582239/59138825-3b61e980-89b1-11e9-9a26-24ac653cdea7.png" width=100></p>
+
+<p><a href="https://www.facebook.com/groups/fullstackwebdevelopment/" rel="nofollow">Facebook Group |</a>
+<a href="https://www.youtube.com/channel/UCqba1eUmmSCal1DbdaHONZA" rel="nofollow">Youtube  |</a>
+<a href="http://www.sumon-it.com" rel="nofollow">Web Site |</a>
+<a href="https://www.facebook.com/csesumonpro" rel="nofollow">Like Us</a></p>
+##Express js & Mysql - Simple CRUD 
+  ###How to use
+  
+  ![express js and mysql crud](https://user-images.githubusercontent.com/29582239/59138726-7d3e6000-89b0-11e9-9701-65618542a0c6.PNG)
+
+  <p>
+    This is Simple CRUD OPEration That means Create/Read/Update/Delete
+    Every Thing is use without vue component only laravel blade file without page load
+  </p>
+  <li> Clone the repository with git clone </li>
+   <li>edit .env file for set your database credentials</li>
+   <li>import sql file in your database</li>
+    <li> Run npm install</li>
+   <li>Run npm start</li>
+   ##Load homepage using this localhost:3000
