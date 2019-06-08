72d43a5db0211dc2ecc97c1b0d8fb9a890f44e31
Sumon sarker <csesumonpro@gmail.com>
2019-06-08 06:11:00 +0600

    404 page added

--- app.js
+++ app.js
@@ -17,20 +17,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRoutes);
 app.use('/users', usersRoutes);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// error handler 404 Page
+app.use((req,res,next)=>{
+  res.status(404).render('404');
 });
 
 module.exports = app;
